75e946f139c724e30d28c351755f172c4d7c2c55
BerenChang <81222862+BerenChang@users.noreply.github.com>
2024-06-30 21:16:42 -0400

    Delete untitled2.m

--- untitled2.m
+++
@@ -1,8 +0,0 @@
-Xvals = [1,2,3,4,5,6,7,8,9];
-Yvals = [2,5,5,5,2,2,5,5,5];
-
-cla()
-hold on
-isGreater = Yvals > 3; 
-scatter(Xvals(~isGreater), Yvals(~isGreater), 50, 'k', 'filled')
-scatter(Xvals(isGreater), Yvals(isGreater), 50, 'r', 'filled')
